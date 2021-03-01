921755d8d1b6e799bf5d6899652fb3c74d6f5236
Anik Biswas <anikbiswas.cse@outlook.com>
2021-03-02 03:34:58 +0600

    Three

--- Address.cs
+++ Address.cs
@@ -6,45 +6,47 @@ using System.Threading.Tasks;
 
 namespace BankAccountApp
 {
-    class Address
+    namespace BankAccountApp
     {
-        private string roadNo;
-        private string houseNo;
-        private string city;
-        private string country;
+        class Address
+        {
+            private string roadNo;
+            private string houseNo;
+            private string city;
+            private string country;
+            public Address(string roadNo, string houseNo, string city, string country)
+            {
+                this.roadNo = roadNo;
+                this.houseNo = houseNo;
+                this.city = city;
+                this.country = country;
+            }
 
-        public Address(string roadNo, string houseNo, string city, string country)
-        {
-            this.roadNo = roadNo;
-            this.houseNo = houseNo;
-            this.city = city;
-            this.country = country;
-        }
+            public string RoadNo
+            {
+                set { this.roadNo = value; }
+                get { return this.roadNo; }
+            }
+            public string HouseNo
+            {
+                set { this.houseNo = value; }
+                get { return this.houseNo; }
+            }
+            public string City
+            {
+                set { this.city = value; }
+                get { return this.city; }
+            }
+            public string Country
+            {
+                set { this.country = value; }
+                get { return this.country; }
+            }
 
-        public string RoadNo
-        {
-            set { this.roadNo = value; }
-            get { return this.roadNo; }
-        }
-        public string HouseNo
-        {
-            set { this.houseNo = value; }
-            get { return this.houseNo; }
-        }
-        public string City
-        {
-            set { this.city = value; }
-            get { return this.city; }
-        }
-        public string Country
-        {
-            set { this.country = value; }
-            get { return this.country; }
-        }
-
-        public void GetAddress()
-        {
-            Console.WriteLine("Address:Road No: {0}, House No: {1}, City: {2}, Country: {3}", this.roadNo, this.houseNo, this.city, this.country);
+            public void GetAddress()
+            {
+                Console.WriteLine("Address:Road No: {0}, House No: {1}, City: {2}, Country: {3}", this.roadNo, this.houseNo, this.city, this.country);
+            }
         }
     }
 }
