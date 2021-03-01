6e141363cf334ed412e654b44295a24bf6a646bc
Anik Biswas <anikbiswas.cse@outlook.com>
2021-03-02 03:31:28 +0600

    3

--- Address.cs
+++ Address.cs
@@ -6,14 +6,45 @@ using System.Threading.Tasks;
 
 namespace BankAccountApp
 {
-    namespace BankAccountApp
+    class Address
     {
-        class Address
+        private string roadNo;
+        private string houseNo;
+        private string city;
+        private string country;
+
+        public Address(string roadNo, string houseNo, string city, string country)
         {
-            private string roadNo;
-            private string houseNo;
-            private string city;
-            private string country;
+            this.roadNo = roadNo;
+            this.houseNo = houseNo;
+            this.city = city;
+            this.country = country;
+        }
+
+        public string RoadNo
+        {
+            set { this.roadNo = value; }
+            get { return this.roadNo; }
+        }
+        public string HouseNo
+        {
+            set { this.houseNo = value; }
+            get { return this.houseNo; }
+        }
+        public string City
+        {
+            set { this.city = value; }
+            get { return this.city; }
+        }
+        public string Country
+        {
+            set { this.country = value; }
+            get { return this.country; }
+        }
+
+        public void GetAddress()
+        {
+            Console.WriteLine("Address:Road No: {0}, House No: {1}, City: {2}, Country: {3}", this.roadNo, this.houseNo, this.city, this.country);
         }
     }
 }
